4c5f34255115bcdb5357836233adfcec1717c52b
Arpan Sahu <arpanrocks95@gmail.com>
2025-01-12 01:39:33 +0530

    added home url to be swagger url

--- event_ticketing_system/urls.py
+++ event_ticketing_system/urls.py
@@ -24,7 +24,7 @@ from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tickets.urls')),
-
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='home'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
